f1dab528549de22be84c6ecdee5a8d831f0cb21e
MinJi <97591103+chaemj97@users.noreply.github.com>
2023-07-22 22:43:47 +0900

    2023-07-22

--- 2023년/백준_골드5_1068_트리.py
+++ 2023년/백준_골드5_1068_트리.py
@@ -1,5 +1,8 @@
 '''
     접근법
+        지울 노드 번호와 관련된 노드들 dfs로 삭제
+        
+        리프노드 : 삭제되지 않은 노드 중 다른 노드의 부모가 아닌 노드
     
 '''
 import sys
@@ -13,3 +16,20 @@ parent = list(map(int,input().split()))
 # 지울 노드 번호
 d = int(input())
 
+def delete_tree(d):
+    # d번 노드 삭제
+    parent[d] = -100
+    # d번 노드의 자식들도 삭제
+    for i in range(n):
+        if parent[i] == d:
+            delete_tree(i)
+
+delete_tree(d)
+
+# 남아있는 리프노드 수
+# 리프노드 : 다른 노드의 부모이면 안된다.
+answer = 0
+for i in range(n):
+    if parent[i] != -100 and i not in parent:
+        answer += 1 
+print(answer)
