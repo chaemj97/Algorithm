89d1ad826b62ddc8c734f5b115f555432aa7af33
MinJi <97591103+chaemj97@users.noreply.github.com>
2023-07-23 23:10:59 +0900

    2023-07-23

--- 2023년/백준_골드3_14658_하늘에서_별똥별이_빗발친다.py
+++ 2023년/백준_골드3_14658_하늘에서_별똥별이_빗발친다.py
@@ -4,14 +4,22 @@
         모서리에 부딪혀도 튕겨내니 트램펄린의 크기를 (l+1) x (l+1)로 보기
 '''
 import sys
-input = sys.stdin.readline
+input = sys.stdin.readline 
 
-n,m,l,k = map(int,input().split())
+# 가로 세로 트램펄린 길이 별똥별수
+n, m, l, k = map(int, input().split())
 
-star = [list(map(int,input().split())) for _ in range(k)]
+arr = [list(map(int, input().split())) for _ in range(k)]
+max_cnt = 0
 
-for a in range(k):
-    for b in range(k):
+for i in range(k):
+    for j in range(k):
         cnt = 0
-        for c in range(k):
-            pass
+        r = arr[i][0]
+        c = arr[j][1]
+        for x in range(k):
+            if r <= arr[x][0] <= r + l and c <= arr[x][1] <= c + l:
+                cnt += 1
+        max_cnt = max(max_cnt, cnt)
+
+print(k - max_cnt)
